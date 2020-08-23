2aeb6571a11d6ad7c5a33c246864d09ef8960936
sanket <sanket.naik@luxactuaries.com>
2020-08-23 10:09:53 +0530

    Updates

---
+++ precache-manifest.b8d8381b4e2dbc899e78a5ce8b2eb221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cf08cd9e4aa6940acd8f16b39838b2c",
+    "url": "/TypeRacer-Socket-client-ReactJs/index.html"
+  },
+  {
+    "revision": "3aab3f6336d499723a38",
+    "url": "/TypeRacer-Socket-client-ReactJs/static/css/main.297d6481.chunk.css"
+  },
+  {
+    "revision": "08b9f73a0c7a9547c488",
+    "url": "/TypeRacer-Socket-client-ReactJs/static/js/2.bf90cca1.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/TypeRacer-Socket-client-ReactJs/static/js/2.bf90cca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aab3f6336d499723a38",
+    "url": "/TypeRacer-Socket-client-ReactJs/static/js/main.27729dd1.chunk.js"
+  },
+  {
+    "revision": "b0dd9b2d99d1f26b7bcc",
+    "url": "/TypeRacer-Socket-client-ReactJs/static/js/runtime-main.45ff35fc.js"
+  }
+]);
